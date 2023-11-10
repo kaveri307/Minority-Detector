ab9b6e520ec3811393b4d25d6a5f369fbfcdf29b
Kaveri Sangamnerkar <145654196+kaveri307@users.noreply.github.com>
2023-11-10 17:00:48 +0530

    Add files via upload

---
+++ Minority_detector.v
@@ -0,0 +1,13 @@
+module Minority_detector(a,b,c,f);
+
+    input a,b,c;
+    output f;
+    
+    wire w1,w2,w3;
+    
+    assign w1=a&b;
+    assign w2=b&c;
+    assign w3=c&a;
+    assign f=~(w1|w2|w3);
+    
+endmodule
